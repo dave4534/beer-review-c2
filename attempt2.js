f8c641b7eaeb8d7e941049cd82fadec620e47ea4
uDave <uDave@Dave.local>
2016-11-13 10:01:35 +0200

    updated

--- attempt2.js
+++ attempt2.js
@@ -21,7 +21,7 @@ var beers = [
     var newBeer = {
       name: name,
       category: category,
-      rating: ratingArray
+      rating: rating
     };
 
     beers.push(newBeer);
@@ -30,15 +30,14 @@ var beers = [
   // Calibrates the beer-list ul and ratingArray, then loops through and 
   // re-posts the entire rating array at once as li's to the ul
   var updateBeers = function(){
-    $('.beers-list').empty()
+    $('.beers-list').empty();
     ratingArray = [];
     for(i = 0; i < beers.length; i++){
       ratingArray.push(beers[i].rating);
-      $('.beers-list').append('<li>' + beers[i].name + '-' + beers[i].category + '-' + beers[i].rating + '</li>')
+      $('.beers-list').append('<li>' + beers[i].name + '-' + beers[i].category + '-' + beers[i].rating + '</li>');
     }
+  };
     
-
-
 // Clicking on Post button defines arguments needed to 
 // invoke addBeer function. Then the addBeer function is invoked with 
 // those arguments. Then updateBeers is invoked, which calibrates and 
@@ -46,18 +45,21 @@ var beers = [
 $('.post-beer').on("click", function(){
   var name = $('.beer-input').val();
   var category = $('.category-input').val();
-  var rating = $('#rating option:selected').val()
+  var rating = $('#rating option:selected').val();
   addBeer(name, category, rating);
   updateBeers();
 });
 
 
-
-//loop through ratingArray (ratings) and print the ul sorted by rating
-// $('.rank').on("click", function(){
-//     // var sortedArray = beers[i].sort();
-//     alert('Hi');
-// });
+// loop through ratingArray (ratings) and print the ul sorted by rating
+$('.rank').on("click", function(){
+  //empty the beer-list ul and append the li's in descending order
+  // 
+  alert(ratingArray.sort(function(a, b){
+    return b-a;
+  })
+  );
+});
 
 //bubble sort
 
